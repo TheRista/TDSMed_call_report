cfdebe178604f32520cd83fc41d88815f126a8dd
TheRista <rista@rista.me>
2018-04-06 11:55:27 +0200

    Add files via upload

---
+++ TDSMed Call report/src/Report.java
@@ -0,0 +1,44 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Button;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+
+public class Report {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Report window = new Report();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Report() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
